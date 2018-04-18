9e725a669565053b61a55894a6da7b9a2d63c40b
Rayhan-Alif <tjiwikimia114@gmail.com>
2018-04-18 16:06:40 +0700

    Fast Tavel

---
+++ rute.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 18, 2018 at 11:02 AM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fasttravel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rute`
+--
+
+CREATE TABLE `rute` (
+  `id` int(10) NOT NULL,
+  `depart_at` varchar(30) NOT NULL,
+  `rute_from` varchar(20) NOT NULL,
+  `rute_to` varchar(20) NOT NULL,
+  `price` int(20) NOT NULL,
+  `transportationid` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
